8156acfee850f760d65e66c18310a70d7ae2e1f3
ReilAvion <47872832+ReilAvion@users.noreply.github.com>
2019-02-22 09:26:59 +0700

    Update Bsearch.kt

--- Bsearch.kt
+++ Bsearch.kt
@@ -1,27 +1,23 @@
 import java.util.Scanner
-import java.util.Arrays;
+import java.util.Arrays
 
+fun main(Args: Array<String>) {
 
-fun main(Args:Array<String>) {
-
-    var angka:Int
-    var size:Int
+    var angka: Int
+    var size: Int
     println("Program Binary Search")
     println()
     println("Kumpulan data Array of Random")
     var notfound = true
-    val sc = Scanner(System.`in`)
+    val sc: Scanner = Scanner(System.`in`)
 
     print("Masukan Ukuran Array = ")
     size = sc.nextInt()
-    var intarray = DoubleArray(size)
+    var intarray = IntArray(size)
     for (i in 0 until size)
-    {
-        intarray[i] = (size) as Double
-    }
+        intarray[i] = (size)
     Arrays.sort(intarray)
-    for (j in 0 until size)
-    {
+    for (j in 0 until size) {
     }
     print("\nMasukan angka yang ingin dicari = ")
     angka = sc.nextInt()
@@ -29,39 +25,35 @@ fun main(Args:Array<String>) {
 
     var batasAtas = intarray.size - 1
     var batasBawah = 0
-    while (notfound == true)
-    {
+    while (notfound == true) {
         val posisiSekarang = (batasAtas + batasBawah) / 2
-        if (intarray[posisiSekarang] == angka.toDouble())
-        {
+        if (intarray[posisiSekarang] == angka.toInt()) {
             notfound = false
-            println("Angka " + angka + " ditemukan")
-        }
-        else if (batasBawah > batasAtas)
-        {
-            println("Angka " + angka + " tidak ditemukan")
+            print("Angka ")
+            print(angka)
+            println(" ditemukan")
+        } else if (batasBawah > batasAtas) {
+            print("Angka ")
+            print(angka)
+            println(" tidak ditemukan")
             break
-        }
-        else
-        {
-            if (intarray[posisiSekarang] < angka)
-            {
-                 batasBawah = posisiSekarang + 1
-            }
-            else
-            {
-                 batasAtas = posisiSekarang - 1
+        } else {
+            if (intarray[posisiSekarang] < angka) {
+                batasBawah = posisiSekarang + 1
+            } else {
+                batasAtas = posisiSekarang - 1
             }
         }
     }
-    if (size <= 10000)
-    {
+    if (size <= 10000) {
         val estimated1 = System.currentTimeMillis() - startTime
-        println("Waktu eksekusi = " + estimated1 + " Milisekon")
-    }
-    else
-    {
+        print("Waktu eksekusi = ")
+        print(estimated1)
+        println(" Milisekon")
+    } else {
         val estimated2 = (System.currentTimeMillis() - startTime) * 0.001
-        println("Waktu eksekusi = " + estimated2 + " sekon")
+        print("Waktu eksekusi = ")
+        print(estimated2)
+        println(" Sekon")
     }
 }
